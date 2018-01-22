e882562089dd4c96a00870fe126c2d4776ae09f4
Haifeng Zhang <haifeng.zhang@adbridgehq.com>
2018-01-22 21:39:02 +0800

    update run_jenkins.sh

--- aliyun/run_jenkins.sh
+++ aliyun/run_jenkins.sh
@@ -1,1 +1,1 @@
-docker run -d --name jenkins01 -p 8080:8080 -p 50000:50000 -e JENKINS_JAVA_OPTIONS="-Duser.timezone=Asia/Shanghai" -e DOCKER_HOST=tcp://172.17.0.1:2375 -v /root/.docker:/root/.docker -v /data/jenkins_data:/root/.jenkins jenkins-ci
+docker run -d --name jenkins01 -p 8080:8080 -p 50000:50000 -e JENKINS_JAVA_OPTIONS="-Duser.timezone=Asia/Shanghai" -e DOCKER_HOST=tcp://172.17.0.1:2375 -v /root/.docker:/root/.docker -v /data/jenkins_data:/root/.jenkins henryzhang1/jenkins-ci:aliyun
